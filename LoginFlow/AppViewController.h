521c6d498c63700d0b330387f0dfc038ec36704d
Angel Campina <acamcor@hotmail.com>
2015-05-16 20:40:30 +0200

    Empty view controller for app screen

---
+++ LoginFlow/AppViewController.h
@@ -0,0 +1,13 @@
+//
+//  AppViewController.h
+//  LoginFlow
+//
+//  Created by Angel Campiña on 15/05/15.
+//  Copyright (c) 2015 Angel Campiña. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppViewController : UIViewController
+
+@end
---
+++ LoginFlow/AppViewController.m
@@ -0,0 +1,13 @@
+//
+//  AppViewController.m
+//  LoginFlow
+//
+//  Created by Angel Campiña on 15/05/15.
+//  Copyright (c) 2015 Angel Campiña. All rights reserved.
+//
+
+#import "AppViewController.h"
+
+@implementation AppViewController
+
+@end
